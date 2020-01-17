0560238357882bed036d1b9f053b11b718fa5673
StepanNovikov <stepan.novikov.1996@gmail.com>
2020-01-17 18:52:51 +0300

    fix bug

--- script.js
+++ script.js
@@ -3,88 +3,91 @@ function load(){
     const form = document.querySelector("form"),
           clear = document.getElementById("clear"),
           ul = document.querySelector("ul"),
-          sortName =document.getElementById("sortName"),
-          sortPriority = document.getElementById("sortPriority")
+          sortName = document.getElementById("sortName"),
+          sortPriority = document.getElementById("sortPriority");
+          
     form.addEventListener('submit', function(event){
         event.preventDefault();
         let input = document.querySelector("input"),
             select = document.getElementById("select").selectedIndex,
-            options = document.getElementById("select").options
+            options = document.getElementById("select").options;
 
-        if(options[select].text == 'Select Priority'){
-            alert("Выберите приоритет задачи!")
-        } else if(input.value == '') {
-            alert("Задача не может быть пустой!")
+        if(options[select].text === 'Select Priority'){
+            alert("Выберите приоритет задачи!");
+        } else if(input.value === '') {
+            alert("Задача не может быть пустой!");
         } else {
-            addTask(input.value, options[select].text)
+            addTask(input.value, options[select].text);
         }
 
     })
 
     clear.addEventListener('click', function(){
         const ul = document.querySelector("ul"),
-              tasksBoard = document.querySelector(".tasksBoard")
-        ul.innerHTML = ''
-        tasksBoard.style.display = 'none'
+              tasksBoard = document.querySelector(".tasksBoard");
+        ul.innerHTML = '';
+        tasksBoard.style.display = 'none';
 
     })
 
     ul.addEventListener("click", function(event){
-        if(event.target.className == 'delete'){
-            deleteTask(event)
+        if(event.target.className === 'delete'){
+            deleteTask(event);
         } else {
-            tickTask(event)
+            tickTask(event);
         }
     })
     
 }
 
 sortName.addEventListener('click', function(){
-    const list = document.querySelector(".sortList")
-    let getLi,shouldSwitch
-    let switching = true
-    let i
+    const list = document.querySelector(".sortList");
+    let li = "";
+    let shouldSwitch = "";
+    let switching = true;
+    let i = 0;
+
     while(switching){
         switching = false;
-        getLi = list.getElementsByTagName("LI")
-        getLabel = list.getElementsByTagName("label")
-        console.log(getLabel[0].innerHTML)
-        for( i = 0; i < (getLabel.length );i++){
-            shouldSwitch = false
-            //console.log(getLi[i].innerHTML.toLowerCase())
-            if (getLabel[i].innerHTML.toLowerCase() > getLabel[i + 1].innerHTML.toLowerCase()) {
-                shouldSwitch = true
-                break
+        li = list.getElementsByTagName("LI");
+        let label = list.getElementsByTagName("label");
+        for( i = 0; i < label.length-1; i++){
+            shouldSwitch = false;
+
+            if (label[i].textContent> label[i + 1].textContent) {
+                shouldSwitch = true;
+                break;
             }
         }
         if(shouldSwitch){
-            getLi[i].parentNode.insertBefore(getLi[i + 1], getLi[i])
-            switching = true
+            li[i].parentNode.insertBefore(li[i + 1], li[i]);
+            switching = true;
         }
     }
 })
 
 function addTask(task,priority){
     const ul = document.querySelector("ul"),
-          li = document.createElement("li")
+          li = document.createElement("li");
 
-    li.innerHTML = `<span class="delete">х</span><span class="priority">Priority: ${priority}</span><input type="checkbox"><label class="label">${task}</label>`
-    ul.appendChild(li)
-    document.querySelector(".tasksBoard").style.display = "block"
+    li.innerHTML = `<span class="delete">х</span><span class="priority">Priority: ${priority}</span><input type="checkbox"><label class="label">${task}</label>`;
+    ul.appendChild(li);
+    document.querySelector(".tasksBoard").style.display = "block";
+    document.querySelector(".filterBoard").style.display = "block"
 }
 
 function deleteTask(event){
-    let remove = event.target.parentNode
-    let parentNode = remove.parentNode
-    parentNode.removeChild(remove)
+    let remove = event.target.parentNode;
+    let parentNode = remove.parentNode;
+    parentNode.removeChild(remove);
 }
 function tickTask(event){
-    const task = event.target.nextSibling
+    const task = event.target.nextSibling;
     if(event.target.checked) {
-        task.style.textDecoration = "line-through"
-        task.style.color = "#ff0000"
+        task.style.textDecoration = "line-through";
+        task.style.color = "#ff0000";
     } else {
-        task.style.textDecoration = "none"
-        task.style.color = "#2f4f4f"
+        task.style.textDecoration = "none";
+        task.style.color = "#2f4f4f";
     }
 }
